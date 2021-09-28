0f27776e10649c0b4b5c75c7067831d0c02c2176
Lee Eun Deok <pleed0215@gmail.com>
2021-09-28 09:28:19 +0900

    Hello

--- lib/file_fruits.dart
+++ lib/file_fruits.dart
@@ -14,6 +14,34 @@ class FruiteApp extends StatefulWidget {
 class _FruiteAppState extends State<FruiteApp> {
   final String _key = 'first';
   final String _fileName = '/fruits.txt';
+  List<String> itemList = List.empty(growable: true);
+  TextEditingController? _todoItemController;
+
+  @override
+  void initState() {
+    super.initState();
+    initData();
+    _todoItemController = TextEditingController();
+  }
+
+  void initData() async {
+    try {
+      var result = await readListFile();
+      print(result);
+      setState(() {
+        itemList.addAll(result);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void writeListFile() async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File(dir.path + _fileName).readAsString();
+    file = file + '\n' + _todoItemController!.value.text;
+    File(dir.path + _fileName).writeAsStringSync(file);
+  }
 
   Future<List<String>> readListFile() async {
     List<String> itemList = List.empty(growable: true);
@@ -21,8 +49,9 @@ class _FruiteAppState extends State<FruiteApp> {
     bool? firstCheck = pref.getBool(_key);
     var dir = await getApplicationDocumentsDirectory();
     bool fileExist = await File(dir.path + _fileName).exists();
+    print(fileExist);
 
-    if (firstCheck == null || firstCheck == false || fileExist) {
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
       pref.setBool(_key, true);
       var file =
           await DefaultAssetBundle.of(context).loadString('assets/fruits.txt');
@@ -46,6 +75,29 @@ class _FruiteAppState extends State<FruiteApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('File example')),
+      body: Center(
+          child: Column(children: [
+        TextField(
+          controller: _todoItemController,
+          keyboardType: TextInputType.text,
+        ),
+        Expanded(
+            child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+                child: Center(
+                    child: Text(itemList[index],
+                        style: const TextStyle(fontSize: 30))));
+          },
+          itemCount: itemList.length,
+        ))
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
